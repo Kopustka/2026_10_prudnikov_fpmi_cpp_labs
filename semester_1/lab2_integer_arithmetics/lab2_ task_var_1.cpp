8602b3020f501c78218b139ae03a727c8cf2fa45
Kopuska <kprudnikov2008@gmail.com>
2026-09-15 22:59:13 +0300

    вторая лабораторная первый вариант

---
+++ semester_1/lab2_integer_arithmetics/lab2_ task_var_1.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int n, k;
+    int buffer;
+    int i = 1;
+    int rank = 1;
+
+    std::cout << "Enter natural number: ";
+
+    if (!(std::cin >> n) || n <= 0) {
+        std::cout << "You must enter a positive number!\n";
+        std::exit(-1);
+    }
+
+    k = n;
+    buffer = 9;
+
+    // цикл для нахождения мин числа 
+    for (;n != 0;) {
+        if ((n % 10) <= buffer) {
+            buffer = n % 10;
+        }
+        n /= 10;
+        i++;
+    }
+
+    // цикл который проходиться по первоначальному числу и отсеивает мин число
+    for (;i != 1;) {
+        if ((k % 10) != buffer) {
+            n += rank * (k % 10);
+            rank *= 10;
+        }
+        k /= 10;
+        i--;
+    }
+
+
+    std::cout << "Its your answer:" << n;
+
+    return 0;
+}
+
+
+
+
